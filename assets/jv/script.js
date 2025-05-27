75be016b4aeb0a42952449788835d2af0d2f6040
LOISA RITCHER <cybermkiozne@gmail.com>
2025-05-27 13:24:11 +0800

    update

--- assets/jv/script.js
+++ assets/jv/script.js
@@ -1,5 +1,9 @@
 console.log("Live background running...");
 
+
+
 document.getElementById("enterButton").addEventListener("click", function () {
   window.location.href = "https://mkxducks.github.io/MSF/mkiozne.html";
 });
+
+
