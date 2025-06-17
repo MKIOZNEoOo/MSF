28b0ac05b997189e5d8449aa3dd1f437fd7130b0
MICHHYYY LOVE <cybermkiozne@gmail.com>
2025-06-17 20:56:15 +0800

    Update script.js

--- assets/jv/script.js
+++ assets/jv/script.js
@@ -1,7 +1,7 @@
 console.log("Live background running...");
 
 document.getElementById("enterButton").addEventListener("click", function () {
-  window.location.href = "https://mkxducks.github.io/MSF/mkiozne.html";
+  window.location.href = "https://mkioznexfile.github.io/MSF/mkiozne.html";
 });
 
 
