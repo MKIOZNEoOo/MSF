8f4666990ed86b0813930185b76a0668bc347042
MKIOZNE (oOo) <cybermkiozne@gmail.com>
2025-11-03 00:05:44 +0800

    Update script.js

--- assets/jv/script.js
+++ assets/jv/script.js
@@ -1,14 +1,13 @@
 console.log("Live background running...");
 
 document.getElementById("enterButton").addEventListener("click", function () {
-  window.location.href = "https://mkiozneooo.github.io/mkweb/mkiozne.html";
+  window.location.href = "https://mkiozneooo.github.io/MSF/mkiozne.html";
 });
 
 function toggleMode() {
   document.body.classList.toggle("dark-mode");
   document.body.classList.toggle("light-mode");
 }
-
 
 
 
